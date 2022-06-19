63cca0b953a9f202866e952bfa8a1bc214da0756
Robert <rob5300@hotmail.com>
2022-06-19 20:11:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,193 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Numerics;
+using System.Text;
+
+Console.WriteLine("Spritesheet Slicer by rob5300.");
+
+//Get target file/folder from args, or ask user to enter it.
+string target = null;
+if(args.Length >= 1)
+{
+    target = args[0];
+}
+if(string.IsNullOrEmpty(target))
+{
+    Console.WriteLine("-= Enter file path or folder path:");
+    target = Console.ReadLine().Replace("\"", "");
+}
+
+//Ask if global dimension should be used for all images or to ask per image.
+SizeInput sharedInput = null;
+if(args.Length >= 4)
+{
+    try
+    {
+        var enumNames = new List<string>(Enum.GetNames(typeof(SizeType)));
+        SizeType type = (SizeType)enumNames.FindIndex(0, x => x.ToLower() == args[1]);
+        int x = Convert.ToInt32(args[2]);
+        int y = Convert.ToInt32(args[3]);
+        sharedInput = new() { type = type, w = x, h = y };
+    }
+    catch
+    {
+        Console.WriteLine("Failed to parse args, should be in the format: 'filepath' 'grid / dimensions' 'x' 'y'");
+        return;
+    }    
+}
+
+if(sharedInput == null)
+{
+    Console.WriteLine("-= Use same output size for all images?");
+    string answer = Console.ReadLine().ToLower();
+    if (answer == "yes" || answer == "y")
+    {
+        sharedInput = GetSizeFromUser();
+    }
+}
+
+if (string.IsNullOrEmpty(target) || target == "*")
+{
+    target = Directory.GetCurrentDirectory();
+}
+else if (!File.Exists(target) || Directory.Exists(target))
+{
+    target = Path.Combine(Directory.GetCurrentDirectory(), target);
+}
+
+if (File.Exists(target) || Directory.Exists(target))
+{
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    //See if this is a file or folder
+    string[] files;
+    if(Directory.Exists(target))
+    {
+        files = Directory.GetFiles(target, "*.png");
+    }
+    else
+    {
+        files = new string[] { target };
+    }
+
+    Log.WriteLine($"-= Will process '{files.Length}' files.");
+    UpdateFiles(files);
+
+    stopwatch.Stop();
+    Console.WriteLine($"Done! Took {stopwatch.Elapsed}.");
+}
+
+/// <summary>
+/// Update a list of files
+/// </summary>
+void UpdateFiles(string[] fileList)
+{
+    StringBuilder logBuilder = new();
+
+    foreach (string _file in fileList)
+    {
+        ProcessFile(_file, logBuilder);
+        Log.WriteLine(logBuilder.ToString());
+        logBuilder.Clear();
+    }
+}
+
+/// <summary>
+/// Update a single file, using the provided updaters. Write log data to given StringBuilder.
+/// </summary>
+void ProcessFile(string file, StringBuilder logBuilder)
+{
+    logBuilder.AppendLine($"\n-= Processing File: '{file}'");
+
+    Bitmap image = new Bitmap(file);
+
+    SpritesheetSlicer slicer = new(image, Path.GetFileNameWithoutExtension(file));
+    slicer.logBuilder = logBuilder;
+
+    SizeInput size = sharedInput;
+
+    if (sharedInput == null)
+    {
+        //Ask user for size if needed
+        Log.WriteLine(logBuilder.ToString());
+        logBuilder.Clear();
+        size = GetSizeFromUser();
+    }
+
+    var images = slicer.SliceUsingInput(size);
+
+    string fileDir = Path.GetDirectoryName(file);
+    string newPathDir = Path.Combine(fileDir, slicer.Name);
+
+    if (!Directory.Exists(newPathDir))
+    {
+        Directory.CreateDirectory(newPathDir);
+    }
+
+    slicer.WriteImagesToDisk(newPathDir, images);
+    slicer.WriteMKSForSprites(images.Count, newPathDir);
+
+    logBuilder.AppendLine($"-= Updated '${file}'");
+}
+
+SizeInput GetSizeFromUser()
+{
+    SizeInput sizeinput = new();
+
+    while (sizeinput.type == SizeType.Unknown)
+    {
+        Console.WriteLine("-= Choose Size Type:\n 1: Grid Size.\n 2: Dimensions.");
+        string input = Console.ReadLine();
+        try
+        {
+            int inputNum = Convert.ToInt32(input);
+            if (inputNum > 0 && inputNum <= 2)
+            {
+                sizeinput.type = (SizeType)inputNum;
+            }
+            else
+            {
+                Console.WriteLine("Invalid type choice, input number.");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Invalid type choice, input number.");
+        }
+    }
+
+    bool sizeValid = false;
+    while (!sizeValid)
+    {
+        Console.WriteLine("-= Input width and height: (e.g. 256x256)");
+        string inputSize = Console.ReadLine();
+        string[] inputSizes = inputSize.Split("x");
+        if (inputSize.Length < 2)
+        {
+            inputSizes = inputSize.Split(" ");
+        }
+
+        try
+        {
+            sizeinput.w = Convert.ToInt32(inputSizes[0]);
+            sizeinput.h = Convert.ToInt32(inputSizes[1]);
+            sizeValid = true;
+        }
+        catch
+        {
+            Console.WriteLine("Could not parse size, input 2 integer numbers seperated by a 'x' or space.");
+        }
+    }
+
+    return sizeinput;
+}
+
+public enum SizeType { Unknown, Grid, Dimensions };
+
+public class SizeInput
+{
+    public SizeType type;
+    public int w;
+    public int h;
+}
---
+++ SpritesheetSlicer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class SpritesheetSlicer
+{
+    public int Cells = 0;
+    public StringBuilder logBuilder { get; set; }
+    public string Name { get; set; }
+
+    private Bitmap spritesheetBitmap;
+
+    public SpritesheetSlicer(Bitmap image, string name)
+    {
+        spritesheetBitmap = image;
+        Name = name;
+    }
+
+    public void WriteImagesToDisk(string directory, List<Bitmap> images)
+    {
+        for (int i = 0; i < images.Count; i++)
+        {
+            var img = images[i];
+            logBuilder.AppendLine($"Wrote image {i} for {Name}");
+            img.Save(Path.Combine(directory, $"{Name}_{i:0000}.png"), ImageFormat.Png);
+        }
+    }
+
+    public List<Bitmap> SliceUsingInput(SizeInput input)
+    {
+        switch (input.type)
+        {
+            case SizeType.Dimensions:
+                return SliceToDimensions(input.w, input.h);
+            case SizeType.Grid:
+                return SliceToGridSize(input.w, input.h);
+            default:
+                throw new ArgumentException("Provided input SizeType was not valid.");
+        }
+    }
+
+    public List<Bitmap> SliceToDimensions(int widthpx, int heightpx)
+    {
+        return SliceTo(new (spritesheetBitmap.Width / widthpx, spritesheetBitmap.Height / heightpx), new (widthpx, heightpx));
+    }
+
+    public List<Bitmap> SliceToGridSize(int width, int height)
+    {
+        return SliceTo(new (width, height), new(spritesheetBitmap.Width / width, spritesheetBitmap.Height / height));
+    }
+
+    private List<Bitmap> SliceTo(Vector2 grid, Vector2 sliceSize)
+    {
+        if (spritesheetBitmap == null) return null;
+
+        int cells = Cells;
+        if (cells <= 0)
+        {
+            cells = (int)(grid.X * grid.Y);
+        }
+        List<Bitmap> slicedImages = new List<Bitmap>(cells);
+
+        for (int i = 0; i < cells; i++)
+        {
+            int currentFrame = i;
+            int yRow = (int)Math.Floor(currentFrame / grid.X);
+            Vector2 rowColumn = new(currentFrame % grid.X, yRow);
+
+            int startX = 0;
+            int startY = 0;
+            if(rowColumn.X > 0)
+            {
+                startX = (int)(sliceSize.X * rowColumn.X);
+            }
+            if (rowColumn.Y > 0)
+            {
+                startY = (int)(sliceSize.Y * rowColumn.Y);
+            }
+
+            var slicedImage = SliceSingle((int)sliceSize.X, (int)sliceSize.Y, startX, startY);
+            if(slicedImage != null)
+            {
+                slicedImages.Add(slicedImage);
+            }
+        }
+        logBuilder.AppendLine($"Spliced image '{Name}' into '{cells}' seperate images");
+        return slicedImages;
+    }
+
+    public void WriteMKSForSprites(int cellCount, string directory)
+    {
+        using (var stream = File.CreateText(Path.Combine(directory, $"{Name}.mks")))
+        {
+            stream.WriteLine("sequence 0");
+
+            for(int i = 0; i < cellCount; i++)
+            {
+                stream.WriteLine($"frame {Name}_{i:0000}.png 1");
+            }
+        }
+    }
+
+    private Bitmap? SliceSingle(int width, int height, int startX, int startY)
+    {
+        logBuilder.AppendLine($"Creating slice ({width}x{height}) at x:{startX} y:{startY}");
+        Bitmap slicedBitmap = new Bitmap(width, height);
+        bool allTransparent = true;
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                Color color = spritesheetBitmap.GetPixel(startX + x, startY + y);
+                if (color.A != 0) allTransparent = false;
+                slicedBitmap.SetPixel(x, y, color);
+            }
+        }
+        return allTransparent ? null : slicedBitmap;
+    }
+}
